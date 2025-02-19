de5f40af940a9773636597eae448e4f7141719dc
nartana2003 <167484053+nartana2003@users.noreply.github.com>
2025-02-19 22:12:38 +1000

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,39 @@
+#Loops
+1.For loop
+
+(i)list_of_names = ['A', 'B', 'C']
+for name in list_of_names:
+    print(name)
+
+(ii)for i in range(10):
+    print(i)
+
+
+(iii)for i in range(len(list_of_names)):
+    print(f"Item in index = {i} is {list_of_names[i]}") #0
+
+(iv)start = len(list_of_names) - 1
+end = -1
+for i in range(start,end, -1):
+    print(f"Item in index = {i} is {list_of_names[i]}") #0
+
+
+2.While loop
+
+(i) while a<10:
+
+
+(ii)while True:
+  break 
+
+(iii)  continue : Ends a iteration of a Loop 
+ break : Ends the Loop
+
+#Lists
+
+(i)list.count(x)- counts the number of times an element appears in the list
+
+(ii)list.append(x)-adds a single element to the list at a time
+
+(iii)list.extend(x+y)-adds multiple elements to a list at a time
 
